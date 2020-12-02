e22b8afbecb99e744caefe571b87d02b34b6521b
j-sattler <mail@sattlerjoshua.de>
2020-12-02 21:03:58 +0100

    Outsource day 2 part 1

--- cmd/main.go
+++ cmd/main.go
@@ -1,56 +1,8 @@
 package main
 
-import (
-	"fmt"
-	"io/ioutil"
-	"log"
-	"strconv"
-	"strings"
-)
-
-type Policy struct {
-	min, max int
-	char string
-	pwd string
-}
+import "github.com/j-sattler/adventofcode-2020/internal/aoc"
 
 func main(){
-	content, err := ioutil.ReadFile("assets/input-2")
-	var counter int
-
-	if err != nil { log.Fatal(err) }
-
-	// split input by new line
-	strArr := strings.Split(string(content), "\n")
-
-	for _, s := range strArr {
-		policy := parsePolicy(s)
-		if policy.isValid() { counter++ }
-	}
-	fmt.Printf("%d passwords are valid\n", counter)
+	aoc.Day2Part1()
 }
 
-func parsePolicy(pStr string) *Policy {
-	splitBySpace := strings.Split(pStr, " ")
-	minMax := strings.Split(splitBySpace[0], "-")
-	char := strings.Split(splitBySpace[1], ":")[0]
-	passwd := splitBySpace[2]
-
-	min, err:= strconv.Atoi(minMax[0])
-	if err != nil { log.Fatal(err) }
-
-	max, err := strconv.Atoi(minMax[1])
-	if err != nil { log.Fatal(err) }
-
-	return &Policy {
-		min:  min,
-		max:  max,
-		char: char,
-		pwd:  passwd,
-	}
-}
-
-func (p *Policy) isValid() bool {
-	count := strings.Count(p.pwd, p.char)
-	return count >= p.min && count <= p.max
-}
