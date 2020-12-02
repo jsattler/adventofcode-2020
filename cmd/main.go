ab93d7e2435937401f1463f24fa67d9ff9cc2a11
j-sattler <mail@sattlerjoshua.de>
2020-12-02 20:17:21 +0100

    Solve part 1 of day 2

--- cmd/main.go
+++ cmd/main.go
@@ -8,43 +8,49 @@ import (
 	"strings"
 )
 
+type Policy struct {
+	min, max int
+	char string
+	pwd string
+}
+
 func main(){
-	content, err := ioutil.ReadFile("assets/input-1")
+	content, err := ioutil.ReadFile("assets/input-2")
+	var counter int
 
 	if err != nil { log.Fatal(err) }
 
-	// split written values into string array
+	// split input by new line
 	strArr := strings.Split(string(content), "\n")
 
-	// iterate over array
-	for i, s1 := range strArr[:len(strArr)-1] {
-
-		first, err := strconv.Atoi(s1)
-
-		if err != nil { log.Fatal(err) }
-		if first > 2020 { continue }
-		// iterate over array start from i + 1
-		for j, s2 := range strArr[i+1:len(strArr)-1] {
-
-			second, err := strconv.Atoi(s2)
-
-			if err != nil { log.Fatal(err) }
-
-			if first + second > 2020 {
-				continue
-			}
-			// iterate over array start from j + 1
-			for _, s3 := range strArr[j+1:]{
-				third, err := strconv.Atoi(s3)
-
-				if err != nil { log.Fatal(err) }
-
-				if first + second + third == 2020 {
-					fmt.Printf("Found matching values (%d, %d, %d) product: %d\n", first, second, third, first * second * third)
-					return
-				}
-			}
-		}
+	for _, s := range strArr {
+		policy := parsePolicy(s)
+		if policy.isValid() { counter++ }
 	}
-	fmt.Println("Did not find matching values")
+	fmt.Printf("%d passwords are valid\n", counter)
+}
+
+func parsePolicy(pStr string) *Policy {
+	splitBySpace := strings.Split(pStr, " ")
+	minMax := strings.Split(splitBySpace[0], "-")
+	char := strings.Split(splitBySpace[1], ":")[0]
+	passwd := splitBySpace[2]
+
+	min, err:= strconv.Atoi(minMax[0])
+	if err != nil { log.Fatal(err) }
+
+	max, err := strconv.Atoi(minMax[1])
+	if err != nil { log.Fatal(err) }
+
+	return &Policy {
+		min:  min,
+		max:  max,
+		char: char,
+		pwd:  passwd,
+	}
+}
+
+func (p *Policy) isValid() bool {
+	count := strings.Count(p.pwd, p.char)
+	return count >= p.min && count <= p.max
 }
