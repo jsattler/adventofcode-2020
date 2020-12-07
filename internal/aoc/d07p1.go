8eff8c08499cf37aff11fe76bee4c384ee71f56a
j-sattler <mail@sattlerjoshua.de>
2020-12-07 23:51:37 +0100

    Remove prints and comments

--- internal/aoc/d07p1.go
+++ internal/aoc/d07p1.go
@@ -9,11 +9,8 @@ import (
 
 const shinyGold = "shiny gold"
 
-var counter = 0
-
 func Day7Part1() {
 
-	fmt.Println(counter)
 	content, err := ioutil.ReadFile("assets/input-7")
 	if err != nil {
 		log.Fatal(err)
@@ -24,23 +21,14 @@ func Day7Part1() {
 	res := make([]string, 0)
 	res = findValidBags(shinyGold, rules, res)
 
-	// we have collected all rules that contain shiny gold but do not start with it.
-	// now we have to look if the parent bag is contained
-
-	fmt.Println(len(res))
+	fmt.Printf("Solution day 7 part 1: %d\n", len(res))
 
 }
 
-// 1. Check what lines contain bag as a child
-// 2. Take the parent bag of each line that was found and repeat the process
-// 3. Repeat until ..
-
 func findValidBags(valid string, rules []string, res []string) []string {
 
-	fmt.Println("checking for", valid)
 	// iterate over all bag rules
 	for _, rule := range rules {
-		fmt.Println("checking rule", rule)
 		isChecked := false
 
 		for _, r := range res {
@@ -53,11 +41,9 @@ func findValidBags(valid string, rules []string, res []string) []string {
 			continue
 		}
 
-		// check if rule contains 'shiny gold' and does not start with shiny gold
 		if strings.Contains(rule, valid) && !strings.HasPrefix(rule, valid) {
 			// add bag to the valid bags
 			parent := strings.Split(rule, " bags contain ")[0]
-			fmt.Println("Found bag that contains shiny gold", parent)
 			res = append(findValidBags(parent, rules, res), parent)
 		}
 	}
