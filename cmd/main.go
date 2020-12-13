c28bc61289a65d3ff3b64841a67585befff5c3f2
j-sattler <mail@sattlerjoshua.de>
2020-12-13 14:42:37 +0100

    Solve part 1 of day 11

--- cmd/main.go
+++ cmd/main.go
@@ -3,25 +3,38 @@ package main
 import "github.com/j-sattler/adventofcode-2020/internal/aoc"
 
 func main() {
+
 	aoc.Day1Part1()
 	aoc.Day1Part2()
+
 	aoc.Day2Part1()
 	aoc.Day2Part2()
+
 	aoc.Day3Part1()
 	aoc.Day3Part2()
+
 	aoc.Day4Part1()
 	aoc.Day4Part2()
+
 	aoc.Day5Part1()
 	aoc.Day5Part2()
+
 	aoc.Day6Part1()
 	aoc.Day6Part2()
+
 	aoc.Day7Part1()
 	aoc.Day7Part2()
+
 	aoc.Day8Part1()
 	aoc.Day8Part2()
+
 	aoc.Day9Part1()
 	aoc.Day9Part2()
+
 	aoc.Day10Part1()
 	aoc.Day10Part2()
+
+	aoc.Day11Part1()
+	aoc.Day11Part2()
 
 }
---
+++ internal/aoc/d11p1.go
@@ -0,0 +1,121 @@
+package aoc
+
+import (
+	"fmt"
+	"io/ioutil"
+	"log"
+	"strings"
+)
+
+const (
+	floor    = '.'
+	occupied = '#'
+	empty    = 'L'
+)
+
+var stateChange = true
+
+// (.) floor
+// (L) empty seat
+// (#) occupied seat
+// decisions based on number of occupied seats
+// Rules:
+// if seat is empty AND no occupied seats around, then this seat becomes occupied
+// if seat is occupied and four or more seats around are occupied, then seat becomes empty
+// otherwise seats state does not change
+func Day11Part1() {
+	occupiedSeats := 0
+	input, err := ioutil.ReadFile("assets/input-11")
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	currentState := strings.Split(string(input), "\n")
+
+	nextState := make([]string, len(currentState))
+	copy(nextState, currentState)
+	for stateChange {
+		stateChange = false
+
+		for i, row := range currentState {
+			// within this iteration currentState does not get modified
+
+			for j, seat := range row {
+
+				switch seat {
+				case empty:
+					// check empty rule i, j
+					_, occCount := visitNeighbors(i, j, currentState)
+					if occCount == 0 {
+						r := []rune(nextState[i])
+						r[j] = occupied
+						nextState[i] = string(r)
+						stateChange = true
+					}
+				case occupied:
+					// check occupied rule
+					_, occCount := visitNeighbors(i, j, currentState)
+					if occCount-1 >= 4 {
+						r := []rune(nextState[i])
+						r[j] = empty
+						nextState[i] = string(r)
+						stateChange = true
+					}
+				case floor:
+					continue
+				}
+
+			}
+		}
+		copy(currentState, nextState)
+	}
+
+	for _, row := range nextState {
+		occupiedSeats += strings.Count(row, string(occupied))
+	}
+
+	fmt.Println("Solution day 11 part 1", occupiedSeats)
+
+}
+
+// visits each neighbor of the given coordinate and returns number of occupied and free seats
+func visitNeighbors(i, j int, matrix []string) (emptyCount, occCount int) {
+	emptyCount = 0
+	occCount = 0
+
+	startRow := i
+	stopRow := i
+
+	if i > 0 {
+		startRow--
+	}
+
+	if i < len(matrix)-1 {
+		stopRow++
+	}
+
+	for _, row := range matrix[startRow : stopRow+1] {
+		startCol := j
+		stopCol := j
+
+		if j > 0 {
+			startCol--
+		}
+
+		if j < len(row)-1 {
+			stopCol++
+		}
+
+		for _, col := range row[startCol : stopCol+1] {
+
+			if col == empty {
+				emptyCount++
+			}
+			if col == occupied {
+				occCount++
+			}
+		}
+	}
+
+	return emptyCount, occCount
+}
